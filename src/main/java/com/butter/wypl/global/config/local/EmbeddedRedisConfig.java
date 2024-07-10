9c63e85fc8a68f609d1a7155b9871031b71515e9
KIMSEI1124 <workju1124@gmail.com>
2024-07-10 20:09:53 +0900

    Hotfix: fix config
    배포가능하게 환경 변수 수정

--- src/main/java/com/butter/wypl/global/config/local/EmbeddedRedisConfig.java
+++ src/main/java/com/butter/wypl/global/config/local/EmbeddedRedisConfig.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
 
 @Slf4j
-@Profile({"local", "test", "jenkins"})
+@Profile({"local", "test"})
 @Configuration
 public class EmbeddedRedisConfig {
 
