c57f033d890df00b010cc5f970f2168b96b59c24
KIMSEI1124 <workju1124@gmail.com>
2024-07-13 12:36:53 +0900

    Feat: Prod Docker Image Build 수정

--- script/prod-docker-image-build.sh
+++ script/prod-docker-image-build.sh
@@ -9,8 +9,14 @@ echo "⚡ bootJar"
 JAR_FILE=$(find build/libs/ -type f -name "*.jar" | head -n 1)
 echo -e "\n🎯 Target JAR: $JAR_FILE"
 
+if [ -z "$1" ]; then
+  read -p "Enter Harbor Hostname: " HARBOR_HOSTNAME
+else
+  HARBOR_HOSTNAME=$1
+fi
+
 IMAGE_NAME=$(basename "$JAR_FILE" | cut -d '-' -f 1)
 IMAGE_TAG=$(basename "$JAR_FILE" | cut -d '-' -f 2 | cut -d '.' -f 1-3)
 echo -e "\n🐬 Docker Image Build, Version: wypl/$IMAGE_NAME:$IMAGE_TAG"
-docker build -t "wypl/$IMAGE_NAME":"$IMAGE_TAG" .
-docker build -t "wypl/$IMAGE_NAME":latest .
+docker build -t "$HARBOR_HOSTNAME/wypl/$IMAGE_NAME":"$IMAGE_TAG" .
+docker image tag "$HARBOR_HOSTNAME/wypl/$IMAGE_NAME":"$IMAGE_TAG" "$HARBOR_HOSTNAME/wypl/$IMAGE_NAME:latest"
